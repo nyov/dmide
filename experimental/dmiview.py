def97519a18456154117c031f0daf08eb123fa92
SoutoJohn <SoutoJohn@a812e4ac-3740-0410-bb29-cddadda57e64>
2009-02-19 06:07:22 +0000

    Changed IconView to ListCtrl. Much more efficient!
    git-svn-id: http://dmide.googlecode.com/svn@11 a812e4ac-3740-0410-bb29-cddadda57e64

--- experimental/dmiview.py
+++ experimental/dmiview.py
@@ -2,138 +2,80 @@
 
 import sys, os
 import wx
-import wx.lib.scrolledpanel as wxScrolled
 import dmi
 import Image
-from extend import IconExtendedPreview
 
 # --------------------------------------------------------------------
 
-class IconView(wxScrolled.ScrolledPanel):
-	def __init__(self, *args, **kwargs):
-		wxScrolled.ScrolledPanel.__init__(self, *args, **kwargs)
+class IconView(wx.ListCtrl):
+	def __init__(self, root):
+		wx.ListCtrl.__init__(self, root, style = wx.LC_ICON | wx.LC_VIRTUAL | wx.LC_EDIT_LABELS)
 
 		self.initAll()
 		self.initBinds()
 
-		self.SetupScrolling()
-
 	def initAll(self):
-		self.SetBackgroundColour(wx.WHITE)
-
-		self.last_fit = -1
-		self.icons = []
-		self.selected = None
+		self.last_pos = -1
+		self.icons = None
+		self.images = None
+		self.image_list = None
 
 	def initBinds(self):
-		self.Bind(wx.EVT_SIZE, self.OnResize)
-		self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
+		self.Bind(wx.EVT_SIZE, self.OnSize)
+		self.Bind(wx.EVT_LIST_END_LABEL_EDIT, self.OnIconStateEdit)
+		self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnIconSelect)
 
-	def readDMI(self, path):
-		icons = dmi.DMIREAD(path)
-		name = os.path.split(path)[-1]
-		self.GetParent().SetTitle('DMI Viewer - %s' % name)
+	def OnGetItemText(self, index, col):
+		try:
+			return self.icons[index].state
+		except IndexError:
+			return 'IndexError'
 
-		self.Freeze()
+	def OnGetItemImage(self, index):
+		try:
+			return self.images[index]
+		except IndexError:
+			return 'IndexError'
 
-		for icon in icons:
-			self.icons.append(IconViewItem(self, icon))
+	def OnGetItemAttr(self, index):
+		return None
 
-		self.Thaw()
-
-		# callng self.updateDisplay directly hangs the program for a bit for mysterious reasons
-		size = self.GetSize()
-		self.SetSize((size[0] + 1, size[1]))
-		self.last_fit = -1
-		self.SetSize(size)
-
-	def die(self):
-		self.icons = []
-		self.selected = None
-		if self.GetSizer():
-			self.GetSizer().Clear(True)
-
-	def updateDisplay(self):
-		width = self.GetSize()[0]
-		fit = width / (60 + 20)
-		if fit <= 0: fit = 1
-		if self.last_fit == fit:
-			return
-		self.last_fit = fit
-
-		self.Freeze()
-
-		sizer = wx.GridBagSizer(12, 12)
-		sizer.AddMany([(item, (index / fit, index % fit), (1, 1), wx.EXPAND | wx.ALL, 4) for index, item in enumerate(self.icons)])
-
-		self.Thaw()
-		self.SetSizer(sizer)
-
-	def Select(self, selected = None):
-		if self.selected:
-			self.selected.Select(False)
-		self.selected = selected
-
-	def OnResize(self, event):
+	def OnSize(self, event):
+		# List won't refresh itself
 		event.Skip()
-		self.updateDisplay()
+		last_pos = self.GetItemRect(self.GetItemCount() - 1)
+		if self.last_pos != last_pos:
+			self.last_pos = last_pos
+			self.Refresh(False)
 
-	def OnLeftUp(self, event):
-		event.Skip()
-		self.GetParent().icon_preview.previewIcon(0)
-		self.Select()
+	def OnIconStateEdit(self, event):
+		state, index = event.GetText(), event.GetIndex()
 
-# --------------------------------------------------------------------
+		self.icons[index].state = state
+		self.RefreshItem(index)
 
-class IconViewItem(wx.Panel):
-	def __init__(self, parent, icon):
-		wx.Panel.__init__(self, parent)
+	def OnIconSelect(self, event):
+		index = event.GetIndex()
 
-		self.SetBackgroundColour(wx.WHITE)
-		self.icon = icon
-		self.SetMinSize((60, 47))
+		self.GetParent().icon_preview.previewIcon(self.icons[index])
 
-		self.initAll()
-		self.initConstraints()
-		self.initBinds()
+	def loadDMI(self, DMI):
+		self.icons = dmi.DMIREAD(DMI)
+		self.images = []
+		self.image_list = wx.ImageList(32, 32)
 
-	def initAll(self):
-		label = self.icon.state
-		if len(label) > 10: label = label[:7] + '...'
-		self.label = wx.StaticText(self, wx.ID_ANY, label)
+		def ImageToBitmap(pil_img):
+			wx_img = wx.EmptyImage(pil_img.size[0], pil_img.size[1])
+			wx_img.SetData(pil_img.convert('RGB').tostring())
+			wx_img.SetAlphaData(pil_img.convert('RGBA').tostring()[3::4])
+			return wx_img.ConvertToBitmap()
 
-		image = self.ImageToBitmap(self.icon.icons[0][0])
-		self.image = wx.StaticBitmap(self, wx.ID_ANY, image)
+		for icon in self.icons:
+			img = ImageToBitmap(icon.icons[0][0])
+			self.images.append(self.image_list.Add(img))
 
-	def initConstraints(self):
-		sizer = wx.BoxSizer(wx.VERTICAL)
-		sizer.Add(self.image, 0, wx.ALIGN_CENTER, border = 2)
-		sizer.Add(self.label, 0, wx.ALIGN_CENTER)
-		self.SetSizer(sizer)
-
-	def initBinds(self):
-		self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
-		self.image.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
-		self.label.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
-
-	def ImageToBitmap(self, pil_img):
-		wx_img = wx.EmptyImage(pil_img.size[0], pil_img.size[1])
-		wx_img.SetData(pil_img.convert('RGB').tostring())
-		wx_img.SetAlphaData(pil_img.convert('RGBA').tostring()[3::4])
-		return wx_img.ConvertToBitmap()
-
-	def Select(self, selected = True):
-		if selected:
-			self.GetParent().Select(self)
-			self.Refresh()
-		else:
-			self.Refresh()
-
-	def OnLeftUp(self, event):
-		event.Skip()
-
-		self.GetParent().GetParent().icon_preview.previewIcon(self)
-		self.Select()
+		self.AssignImageList(self.image_list, wx.IMAGE_LIST_NORMAL)
+		self.SetItemCount(len(self.icons))
 
 # --------------------------------------------------------------------
 
@@ -148,19 +90,18 @@ class IconPreview(wx.Panel):
 		self.initAll()
 		self.initConstraints()
 		self.initBinds()
+		self.showAll(False)
 
 	def initAll(self):
 		self.empty_icon = wx.BitmapFromImage(wx.EmptyImage(128, 128, True))
-		self.default_font = wx.Font(12, wx.SWISS, wx.NORMAL, wx.BOLD, False, 'Arial')
 
 		self.icon = wx.StaticBitmap(self, wx.ID_ANY, self.empty_icon, style = wx.SIMPLE_BORDER)
 
 		self.icon_state = wx.StaticText(self, wx.ID_ANY, '', style = wx.ALIGN_CENTER | wx.TEXT_ALIGNMENT_CENTER)
-		self.icon_state.SetMinSize((100, -1))
-		self.icon_state.SetFont(self.default_font)
 
 		self.icon_state_edit = wx.TextCtrl(self, wx.ID_ANY, '', style = wx.TE_PROCESS_ENTER)
-		self.icon_state_edit.Hide()
+
+		self.icon_state.SetMinSize((100, self.icon_state_edit.GetSize()[1]))
 
 		self.static_directions = wx.StaticText(self, wx.ID_ANY, 'Directions:')
 		self.directions = wx.ComboBox(self, wx.ID_ANY, size = (75, -1), value = 'One', choices = ['One', 'Four', 'Eight'])
@@ -177,9 +118,6 @@ class IconPreview(wx.Panel):
 		self.load_dmi = wx.Button(self, wx.ID_ANY, 'Open DMI')
 
 		self.hide_list = [self.static_directions, self.directions, self.static_frames, self.frames, self.static_loops, self.loops, self.rewind, self.expand_icon]
-
-		for hide in self.hide_list:
-			hide.Hide()
 
 	def initConstraints(self):
 		sizer = wx.BoxSizer(wx.VERTICAL)
@@ -211,6 +149,8 @@ class IconPreview(wx.Panel):
 
 		self.SetSizer(sizer)
 
+		sizer.Hide(self.icon_state_edit, True)
+
 	def initBinds(self):
 		self.icon_state.Bind(wx.EVT_LEFT_DOWN, self.OnIconStateLeftDown)
 		self.icon_state_edit.Bind(wx.EVT_KEY_DOWN, self.OnIconStateKeyDown)
@@ -218,41 +158,51 @@ class IconPreview(wx.Panel):
 		self.expand_icon.Bind(wx.EVT_BUTTON, self.OnExpandIconPress)
 		self.load_dmi.Bind(wx.EVT_BUTTON, self.OnLoadDmiPress)
 
-	def previewIcon(self, iconItem):
+	def showAll(self, show = True):
+		for hide in self.hide_list:
+			if show:
+				self.GetSizer().Show(hide, True, True)
+			else:
+				self.GetSizer().Hide(hide, True)
+
+	def previewIcon(self, icon):
 		self.Freeze()
 
-		if iconItem == 0:
+		if icon == 0:
 			self.icon.SetBitmap(self.empty_icon)
 			self.icon.Refresh()
 			self.icon_state.SetLabel('')
 			self.icon_state_edit.SetValue('')
-			self.icon_state.Show()
-			self.icon_state_edit.Hide()
+			self.GetSizer().Show(self.icon_state, True, True)
+			self.GetSizer().Hide(self.icon_state_edit, True)
 
-			for hide in self.hide_list:
-				hide.Hide()
+			self.showAll(False)
 
 		else:
-			icon = iconItem.icon.icons[0][0]
-			icon = icon.resize((128, 128), Image.NEAREST)
-			self.icon.SetBitmap(iconItem.ImageToBitmap(icon))
+			def ImageToBitmap(pil_img):
+				wx_img = wx.EmptyImage(pil_img.size[0], pil_img.size[1])
+				wx_img.SetData(pil_img.convert('RGB').tostring())
+				wx_img.SetAlphaData(pil_img.convert('RGBA').tostring()[3::4])
+				return wx_img.ConvertToBitmap()
 
-			self.icon_state.SetLabel(iconItem.icon.state)
-			self.icon_state_edit.SetValue(iconItem.icon.state)
+			img = icon.icons[0][0].resize((128, 128), Image.NEAREST)
+			self.icon.SetBitmap(ImageToBitmap(img))
+
+			self.icon_state.SetLabel(icon.state)
+			self.icon_state_edit.SetValue(icon.state)
 
 			dirs = {'1': 'One', '4': 'Four', '8': 'Eight'}
 			self.directions.SetValue('One')
-			if str(iconItem.icon.dirs) in dirs:
-				self.directions.SetValue(dirs[str(iconItem.icon.dirs)])
+			if str(icon.dirs) in dirs:
+				self.directions.SetValue(dirs[str(icon.dirs)])
 
-			self.frames.SetValue(iconItem.icon.frames)
+			self.frames.SetValue(icon.frames)
 
-			self.loops.SetValue(iconItem.icon.loops)
+			self.loops.SetValue(icon.loops)
 
-			self.rewind.SetValue(iconItem.icon.rewind)
+			self.rewind.SetValue(icon.rewind)
 
-			for hide in self.hide_list:
-				hide.Show()
+			self.showAll(True)
 
 		self.Layout()
 		self.Thaw()
@@ -265,15 +215,13 @@ class IconPreview(wx.Panel):
 		dlg = wx.FileDialog(self, message = 'Open a DMI...', defaultDir = os.getcwd(), wildcard = wildcard, style = wx.OPEN | wx.CHANGE_DIR)
 		if dlg.ShowModal() == wx.ID_OK:
 			path = str(dlg.GetPaths()[0])
-			self.previewIcon(0)
 
-			window = self.GetParent()
-			window.icon_view.die()
-			window.icon_view.readDMI(path)
+			self.previewIcon(0)
+			self.GetParent().icon_view.loadDMI(path)
 
 	def OnIconStateLeftDown(self, event):
-		self.icon_state.Hide()
-		self.icon_state_edit.Show()
+		self.GetSizer().Hide(self.icon_state, True)
+		self.GetSizer().Show(self.icon_state_edit, True, True)
 		self.icon_state_edit.SetFocus()
 		self.Layout()
 
@@ -299,57 +247,34 @@ class IconPreview(wx.Panel):
 
 class DMIViewer(wx.Frame):
 	def __init__(self):
-		wx.Frame.__init__(self, None, wx.ID_ANY, 'DMI Viewer')
+		wx.Frame.__init__(self, None, title = 'DMI Viewer')
 
-		self.SetMinSize((200 + 300, 370))
 		self.initAll()
 		self.initConstraints()
 		self.Show(True)
+		self.Center()
+
+		self.icon_view.loadDMI('../turfs.dmi')
 
 	def initAll(self):
-		self.icon_preview = IconPreview(self)
 		self.icon_view = IconView(self)
-		self.icon_extend = IconExtendedPreview(self)
+		self.icon_preview = IconPreview(self)
 
 	def initConstraints(self):
-		if self.GetSizer():
-			self.GetSizer().Destroy()
-
 		sizer = wx.BoxSizer(wx.HORIZONTAL)
+		sizer.Add(self.icon_preview, 0, wx.ALL | wx.EXPAND, 4)
+		sizer.Add(self.icon_view, 1, wx.ALL | wx.EXPAND, 4)
+		self.SetSizerAndFit(sizer)
 
-		flag = wx.SizerFlags(0).Expand().Border(wx.LEFT | wx.TOP | wx.BOTTOM, 8)
-
-		sizer.AddF(self.icon_preview, flag)
-		sizer.Add(self.icon_view, 1, wx.ALL | wx.EXPAND, 8)
-		sizer.Add(self.icon_extend, 1, wx.ALL | wx.EXPAND, 8)
-
-		sizer.Hide(self.icon_extend, True)
-
-		self.SetSizer(sizer)
-		self.SetSize((850, 600))
+		self.SetSize(self.GetSize() + (360, 0))
 
 	def initBinds(self):
 		self.Bind(wx.EVT_CLOSE, self.OnClose)
 
 	def OnClose(self, event):
-		self.icon_preview.Destroy()
-		self.icon_view.die()
+		self.GetSizer().Clear(True)
 		self.Destroy()
 		event.Skip()
-
-	def SwapPreviews(self):
-		sizer = self.GetSizer()
-
-		if sizer.IsShown(self.icon_view):
-			sizer.Hide(self.icon_view, True)
-			sizer.Show(self.icon_extend, True, True)
-			icon = self.icon_view.selected.icon
-			self.icon_extend.displayIcon(icon)
-		else:
-			sizer.Hide(self.icon_extend, True)
-			sizer.Show(self.icon_view, True, True)
-
-		self.Layout()
 
 # --------------------------------------------------------------------
 
