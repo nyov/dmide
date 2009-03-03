ac9915dbee3399d8500dcc95a46a2489c14c0312
SoutoJohn <SoutoJohn@a812e4ac-3740-0410-bb29-cddadda57e64>
2009-03-03 22:53:47 +0000

    git-svn-id: http://dmide.googlecode.com/svn@34 a812e4ac-3740-0410-bb29-cddadda57e64

--- core/dmconsole.py
+++ core/dmconsole.py
@@ -20,9 +20,21 @@ class DMConsole(wx.Panel):
 #-------------------------------------------------------------------
 
 	def initAll(self):
-		self.output = wx.TextCtrl(self, style = wx.NO_BORDER | wx.TE_MULTILINE)
+		self.output = wx.TextCtrl(self, style = wx.NO_BORDER | wx.TE_MULTILINE | wx.TE_RICH2)
+		self.output.SetEditable(False)
 
 		self.input = wx.TextCtrl(self, style = wx.TE_PROCESS_ENTER)
+
+		self.style_1 = wx.TextAttr('BLACK', wx.NullColour, wx.Font(8, wx.TELETYPE, wx.NORMAL, wx.NORMAL))
+		self.style_2 = wx.TextAttr('PURPLE', wx.NullColour, wx.Font(8, wx.TELETYPE, wx.NORMAL, wx.BOLD))
+		self.style_3 = wx.TextAttr('RED', wx.NullColour, wx.Font(8, wx.ROMAN, wx.NORMAL, wx.BOLD))
+
+		self.message('INTERACTIVE DEVELOPER CONSOLE. USE CAUTION.', self.style_3)
+		self.message('', self.style_3)
+		self.message('system\n\t%s' % sys.platform, self.style_1)
+		self.message('python\n\t%s' % sys.version, self.style_1)
+		self.message('wx\n\t%s' % wx.__version__, self.style_1)
+		self.message('', self.style_3)
 
 #-------------------------------------------------------------------
 
@@ -43,7 +55,7 @@ class DMConsole(wx.Panel):
 		text = self.input.GetValue()
 		self.input.SetValue('')
 
-		self.output.SetValue(self.output.GetValue() + ('>>> %s' % text) + '\n')
+		self.message('>>> %s' % text, self.style_2)
 
 		self.evaluate(text)
 
@@ -57,6 +69,21 @@ class DMConsole(wx.Panel):
 		except Exception, e:
 			text = repr(e)
 
-		self.output.SetValue(self.output.GetValue() + text + '\n')
+		self.message(text, self.style_1)
+
+#-------------------------------------------------------------------
+
+	def message(self, message, style = None):
+		if not len(message) or message[-1] != '\n':
+			message = '%s\n' % message
+
+		self.output.SetEditable(True)
+		self.output.AppendText(message)
+
+		if style:
+			length = len(self.output.GetValue())
+			self.output.SetStyle(length - len(message), length, style)
+
+		self.output.SetEditable(False)
 
 #-------------------------------------------------------------------
